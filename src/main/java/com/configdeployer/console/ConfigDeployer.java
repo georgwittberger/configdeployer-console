46c4044d9d51710c18f5cd4e4200a6274a48ffca
Georg Wittberger <georg.wittberger@gmail.com>
2014-06-08 12:15:41 +0200

    Added support for new properties variables resolver

--- src/main/java/com/configdeployer/console/ConfigDeployer.java
+++ src/main/java/com/configdeployer/console/ConfigDeployer.java
@@ -10,12 +10,15 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.configdeployer.deployer.ProfileDeployer;
 import com.configdeployer.preparer.EnvironmentVariablesResolver;
+import com.configdeployer.preparer.PreparerException;
 import com.configdeployer.preparer.ProfilePreparer;
+import com.configdeployer.preparer.PropertiesVariablesResolver;
 import com.configdeployer.preparer.SystemPropertiesResolver;
 import com.configdeployer.preparer.VariablesPreparer;
 import com.configdeployer.provider.FileInputStreamProvider;
@@ -26,12 +29,13 @@ import com.configdeployer.provider.ProfileProvider;
 public class ConfigDeployer
 {
 
-    private static final String CLI_SYNOPSIS = "configdeployer [-ev [-vd <num>]] [-ff] profile [profile ...]";
+    private static final String CLI_SYNOPSIS = "configdeployer [-ev [-vd <num>] [-vp <file>]] [-ff] profile [profile ...]";
     private static final String CLI_DESCRIPTION = "Deploys configuration profiles on the current environment.";
     private static final Logger logger = LoggerFactory.getLogger(ConfigDeployer.class);
 
     public static void main(String[] args)
     {
+        // Phase 1: Create the command line options definition.
         Options commandLineOptions = createCommandLineOptions();
         CommandLineParser commandLineParser = new PosixParser();
         CommandLine commandLine = null;
@@ -44,6 +48,8 @@ public class ConfigDeployer
             logger.error("Could not parse command line!", e);
             System.exit(1);
         }
+
+        // Phase 2: Check if provided command line arguments are sufficient.
         if (commandLine.getArgs().length < 1)
         {
             HelpFormatter helpGenerator = new HelpFormatter();
@@ -51,28 +57,68 @@ public class ConfigDeployer
             System.exit(1);
         }
 
+        // Phase 3: Determine deployment configuration from command line.
         boolean enableVariables = commandLine.hasOption(CommandLineOption.ENABLE_VARIABLES.getName());
         boolean failFast = commandLine.hasOption(CommandLineOption.FAIL_FAST.getName());
         int variablesDepth = 1;
-        if (enableVariables && commandLine.hasOption(CommandLineOption.VARIABLES_DEPTH.getName()))
+        String variablesPropertiesFile = null;
+        if (enableVariables)
         {
-            try
+            if (commandLine.hasOption(CommandLineOption.VARIABLES_DEPTH.getName()))
             {
-                variablesDepth = Integer.parseInt(commandLine.getOptionValue(CommandLineOption.VARIABLES_DEPTH
-                        .getName()));
+                try
+                {
+                    variablesDepth = Integer.parseInt(commandLine.getOptionValue(CommandLineOption.VARIABLES_DEPTH
+                            .getName()));
+                }
+                catch (NumberFormatException e)
+                {
+                    logger.error("Argument for parameter '" + CommandLineOption.VARIABLES_DEPTH.getLongName()
+                            + "' is not a valid integer number.", e);
+                    System.exit(1);
+                }
             }
-            catch (NumberFormatException e)
+            if (commandLine.hasOption(CommandLineOption.VARIABLES_PROPERTIES.getName()))
             {
-                logger.error("Argument for parameter '" + CommandLineOption.VARIABLES_DEPTH.getLongName()
-                        + "' is not a valid integer number.", e);
-                System.exit(1);
+                variablesPropertiesFile = commandLine.getOptionValue(CommandLineOption.VARIABLES_PROPERTIES.getName());
             }
         }
-        logger.info("Deployment configuration: {} = {}, {} = {}, {} = {}",
+        logger.info("Deployment configuration: {} = {}, {} = {}, {} = {}, {} = {}",
                 CommandLineOption.ENABLE_VARIABLES.getLongName(), enableVariables,
                 CommandLineOption.VARIABLES_DEPTH.getLongName(), variablesDepth,
+                CommandLineOption.VARIABLES_PROPERTIES.getLongName(), variablesPropertiesFile,
                 CommandLineOption.FAIL_FAST.getLongName(), failFast);
 
+        // Phase 4: Create the profile preparers (reused for every profile).
+        List<ProfilePreparer> profilePreparers = new ArrayList<ProfilePreparer>(1);
+        if (enableVariables)
+        {
+            VariablesPreparer variablesPreparer = new VariablesPreparer();
+            variablesPreparer.setDepth(variablesDepth);
+            variablesPreparer.addResolver("env", new EnvironmentVariablesResolver());
+            variablesPreparer.addResolver("sys", new SystemPropertiesResolver());
+            if (StringUtils.isNotBlank(variablesPropertiesFile))
+            {
+                try
+                {
+                    File propertiesFile = new File(variablesPropertiesFile);
+                    if (!propertiesFile.isFile())
+                    {
+                        logger.error("Properties file for variable substitution not found: " + variablesPropertiesFile);
+                        System.exit(1);
+                    }
+                    variablesPreparer.addResolver("p", new PropertiesVariablesResolver(propertiesFile));
+                }
+                catch (PreparerException e)
+                {
+                    logger.error("Could not create properties variables resolver.", e);
+                    System.exit(1);
+                }
+            }
+            profilePreparers.add(variablesPreparer);
+        }
+
+        // Phase 5: Load and deploy each single configuration profile.
         boolean success = true;
         for (String fileArgument : commandLine.getArgs())
         {
@@ -80,16 +126,6 @@ public class ConfigDeployer
             if (fileArgument.endsWith(".gz") || fileArgument.endsWith(".gzip"))
             {
                 inputStreamProvider = new GZipInputStreamProvider(inputStreamProvider);
-            }
-
-            List<ProfilePreparer> profilePreparers = new ArrayList<ProfilePreparer>(1);
-            if (enableVariables)
-            {
-                VariablesPreparer variablesPreparer = new VariablesPreparer();
-                variablesPreparer.setDepth(variablesDepth);
-                variablesPreparer.addResolver("env", new EnvironmentVariablesResolver());
-                variablesPreparer.addResolver("sys", new SystemPropertiesResolver());
-                profilePreparers.add(variablesPreparer);
             }
 
             try
@@ -109,6 +145,8 @@ public class ConfigDeployer
                 break;
             }
         }
+
+        // Phase 6: Give feedback on the deployment.
         if (success)
         {
             logger.info("DEPLOYMENT SUCCESSFUL");
@@ -137,6 +175,8 @@ public class ConfigDeployer
                 "Enable variable substitution in certain elements of the profile."),
         VARIABLES_DEPTH("vd", "variables-depth", true,
                 "Sets the number of cycles for variable substitution (default = 1)."),
+        VARIABLES_PROPERTIES("vp", "variables-properties", true,
+                "Sets the properties file to use for variable substitution."),
         FAIL_FAST("ff", "fail-fast", false, "Abort immediately if deployment of a profile fails.");
 
         private final String name;
